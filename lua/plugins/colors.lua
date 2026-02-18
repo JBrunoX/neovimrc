124c426dd696b62426a5287e1d87cfcced1c1c06
Justin Bruno <01justin.bruno@gmail.com>
2026-02-17 22:07:49 -0500

    added colorizer

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,24 +1,31 @@
 local function enable_transparency()
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 end
 
 return {
-    {
-	"thesimonho/kanagawa-paper.nvim",
-	lazy = false,
-	priority = 1000,
-	init = function()
-	    vim.cmd.colorscheme("kanagawa-paper")
-	    enable_transparency()
-	end,
-    },
-    {
-	"nvim-lualine/lualine.nvim",
-	dependencies = {
-	    "nvim-tree/nvim-web-devicons",
+	{
+		"thesimonho/kanagawa-paper.nvim",
+		lazy = false,
+		priority = 1000,
+		init = function()
+			vim.cmd.colorscheme("kanagawa-paper")
+			enable_transparency()
+		end,
 	},
-	opts = {
-	    theme = "kanagawa-paper",
-	}
-    },    
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		opts = {
+			theme = "kanagawa-paper",
+		},
+	},
+
+	{
+		"norcalli/nvim-colorizer.lua", -- Displays colors in all file types
+		config = function()
+			require("colorizer").setup()
+		end,
+	},
 }

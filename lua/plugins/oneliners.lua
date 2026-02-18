acc90af8af78b67011320f204150575d6b874333
Justin Bruno <01justin.bruno@gmail.com>
2026-02-17 22:10:49 -0500

    remove unused plugins and replace plugins with better ones

--- lua/plugins/oneliners.lua
+++ lua/plugins/oneliners.lua
@@ -1,12 +1,10 @@
 return {
-    {
-	'ojroques/vim-oscyank', -- Allows copying to clipboard in ssh tunnels
-    },
-    { 
-	'brenoprata10/nvim-highlight-colors', -- Displays colors in .css files
-        config = function()
-            require('nvim-highlight-colors').setup({})
-        end
-    },
+  {
+    "folke/lazydev.nvim",
+    ft = "lua",
+    opts = {}
+  },
+  {
+    "antosha417/nvim-lsp-file-operations"
+  }
 }
-
